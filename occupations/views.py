94cfca6163a91e6c4d610bca7d24e80ca21ca86d
Nicolás Bustamante <nicolas.bustamante@haulmerinc.onmicrosoft.com>
2022-06-07 21:40:05 -0400

    fix(occupations): add search and pagination to institution endpoint

--- occupations/views.py
+++ occupations/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Education, Institution
 from .serializers import EducationSerializer, InstitutionSerializer
+from api.utils import CustomPageNumberPagination
 
 class EducationtListAPIView(generics.ListAPIView):
     queryset = Education.objects.all()
@@ -8,6 +9,11 @@ class EducationtListAPIView(generics.ListAPIView):
     authentication_classes = []
 
 class InstitutionListAPIView(generics.ListAPIView):
-    queryset = Institution.objects.all()
+    queryset = Institution.objects.all().order_by('id')
     serializer_class = InstitutionSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    pagination_class = CustomPageNumberPagination
+    
     authentication_classes = []
